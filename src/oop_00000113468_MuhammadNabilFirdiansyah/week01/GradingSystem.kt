898b1919ef587f32265a5137258b316191f327bb
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-02-03 15:17:25 +0700

    week01 : initial setup and procedural code

---
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_00000113468_MuhammadNabilFirdiansyah.week01
+
+fun main(){
+    var name : String = "John Thor"
+    var score : Int = 80
+
+    println("Nama: " +name +", Nilai: "+ score)
+}
