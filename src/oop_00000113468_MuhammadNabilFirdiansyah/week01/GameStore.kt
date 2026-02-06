9e340ceaf4f672ea039b0e3e46009a0d3a23f3d6
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-02-06 19:36:49 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000113468_MuhammadNabilFirdiansyah/week01/GameStore.kt
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week01/GameStore.kt
@@ -2,5 +2,13 @@ package oop_00000113468_MuhammadNabilFirdiansyah.week01
 
 fun main(){
     val gameTitle ="PUBG"
-    val price =500000
+    val price =700000
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) {
+        (price * 0.2).toInt()
+    }
+    else {
+        (price * 0.1).toInt()
+    }
