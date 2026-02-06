651c2266bdc1696a85e68be07ec74ad38d03a5f1
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-02-06 19:40:42 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000113468_MuhammadNabilFirdiansyah/week01/GameStore.kt
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week01/GameStore.kt
@@ -3,6 +3,9 @@ package oop_00000113468_MuhammadNabilFirdiansyah.week01
 fun main(){
     val gameTitle ="PUBG"
     val price =700000
+    val discount =calculateDiscount(price)
+    val finalPrice= price - discount
+    printReceipt(title = gameTitle, originalPrice = price,finalPrice = finalPrice)
 }
 
 fun calculateDiscount(price: Int): Int =
@@ -12,3 +15,9 @@ fun calculateDiscount(price: Int): Int =
     else {
         (price * 0.1).toInt()
     }
+
+fun printReceipt(title: String, originalPrice: Int ,finalPrice: Int) {
+    println("Judul Game = $title")
+    println("Harga Asli = $originalPrice ")
+    println("Harga Akhir  = $finalPrice")
+}
