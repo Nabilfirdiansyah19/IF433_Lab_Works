6f80ddad2066c2d89680f8ddd0c731760efe2841
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-03-04 20:04:11 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week05/EWallet.kt
@@ -0,0 +1,18 @@
+package oop_00000113468_MuhammadNabilFirdiansyah.week05
+
+class EWallet(accountName: String, var balance: Double) : PaymentMethod(accountName) {
+
+    override fun processPayment(amount: Double) {
+        if (balance >= amount) {
+            balance -= amount
+            println("[$accountName] Pembayaran E-Wallet sebesar $amount berhasil. Sisa saldo: $balance")
+        } else {
+            println("[$accountName] Gagal: Saldo tidak cukup.")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+        println("[$accountName] Top up sebesar $amount berhasil. Saldo sekarang: $balance")
+    }
+}
