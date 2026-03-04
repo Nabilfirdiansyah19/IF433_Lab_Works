2368b7f7b77cb95cfd2a0424a927c8b2e9686ef1
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-03-04 20:07:09 +0700

    week05: (task 2) create CreditCard class

---
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week05/CreditCard.kt
@@ -0,0 +1,14 @@
+package oop_00000113468_MuhammadNabilFirdiansyah.week05
+
+class CreditCard(accountName: String, val limit: Double) : PaymentMethod(accountName) {
+    var usedAmount: Double = 0.0
+
+    override fun processPayment(amount: Double) {
+        if (usedAmount + amount <= limit) {
+            usedAmount += amount
+            println("[$accountName] Pembayaran Kartu Kredit sebesar $amount sukses.")
+        } else {
+            println("[$accountName] Transaksi ditolak: Melebihi limit.")
+        }
+    }
+}
