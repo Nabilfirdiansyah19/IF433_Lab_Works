372075e256488fe9e1dd02ed64dbd7284626a1d0
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-03-10 14:59:46 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000113468_MuhammadNabilFirdiansyah.week06
+
+interface Camera {
+    fun turnOn(){
+        println("lensa kamera terbuka, sensor aktif")
+    }
+}
+
+interface Phone{
+    fun turnOn(){
+        println("sinyal seluler mencari jaringan")
+    }
+}
