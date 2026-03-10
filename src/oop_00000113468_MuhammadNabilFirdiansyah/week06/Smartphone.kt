09e1a051a08baecb6e362be7d71cdfe09f2a7391
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-03-10 15:13:26 +0700

    week06: implement decoupled processCheckout in main

--- src/oop_00000113468_MuhammadNabilFirdiansyah/week06/Smartphone.kt
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week06/Smartphone.kt
@@ -2,7 +2,7 @@ package oop_00000113468_MuhammadNabilFirdiansyah.week06
 
 class Smartphone: Camera, Phone {
     override fun turnOn() {
-        super<Phone>.turnOn()
+        super<Camera>.turnOn()
         super<Phone>.turnOn()
         println("sistem operasi smartphone berhasil booting")
     }
