487897c41ceb06724632b9b6b7b6746436bfbd26
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-03-11 19:12:13 +0700

    week06: (task) create SmartLamp

---
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week06/SmartLamp.kt
@@ -0,0 +1,11 @@
+package oop_00000113468_MuhammadNabilFirdiansyah.week06
+
+class SmartLamp(override val id: String, override val name: String) : SmartDevice, Switchable {
+    override fun turnOn() {
+        println("Lampu $name ($id) dinyalakan: Cahaya hangat memenuhi ruangan.")
+    }
+
+    override fun turnOff() {
+        println("Lampu $name ($id) dimatikan.")
+    }
+}
