43e6044223562a1a749db4e724fa8a9f69649f2e
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-03-11 19:20:13 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week06/SmartHomeHub.kt
@@ -0,0 +1,19 @@
+package oop_00000113468_MuhammadNabilFirdiansyah.week06
+
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+        println("${device.name} berhasil ditambahkan ke Hub.")
+    }
+
+    fun turnOffAllSwitches() {
+        println("\n--- Shutting Down All Devices ---")
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
