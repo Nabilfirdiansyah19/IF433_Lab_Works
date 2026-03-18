062e6599efb789edd30c5b8fc09965ffc96f1707
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-03-18 18:34:52 +0700

    week07: (task) implement factory pattern in WeaponForge

---
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week07/WeaponForge.kt
@@ -0,0 +1,16 @@
+package oop_00000113468_MuhammadNabilFirdiansyah.week07
+
+class Weapon private constructor(val item: GameItem, val durability: Int) {
+
+    companion object {
+        fun forgeStarterSword(): Weapon {
+            val starterItem = GameItem("Pedang Kayu Bapuk", 5, ItemRarity.COMMON)
+            return Weapon(starterItem, 50)
+        }
+
+        fun forgeEpicSword(): Weapon {
+            val epicItem = GameItem("Excalibur Sword", 99, ItemRarity.EPIC)
+            return Weapon(epicItem, 90)
+        }
+    }
+}
