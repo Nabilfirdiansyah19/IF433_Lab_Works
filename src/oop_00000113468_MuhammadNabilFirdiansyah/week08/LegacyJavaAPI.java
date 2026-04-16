e0dbf63d39d59bc7bac1a6d42d1f5480d03c2858
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-04-16 11:07:57 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week08/LegacyJavaAPI.java
@@ -0,0 +1,7 @@
+package oop_00000113468_MuhammadNabilFirdiansyah.week08;
+
+public class LegacyJavaAPI {
+    public static String fetchServerStatus(){
+        return  "server_online_200_ok";
+    }
+}
