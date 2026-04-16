a4c6bd18f98de6f3efe634ab274fbdaed5b3d814
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-04-16 11:10:03 +0700

    week08: implement java interop using !!

--- src/oop_00000113468_MuhammadNabilFirdiansyah/week08/Main.kt
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week08/Main.kt
@@ -52,4 +52,9 @@ fun main(){
     } catch (e: IllegalArgumentException){
         println(e.message)
     }
+
+    println("\n=== TEST JAVA INTEROP ===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
+    val statusLength = javaResponse!!.length
+    println("status dari java : $javaResponse(length : $statusLength)")
 }
