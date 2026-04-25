8e975d78df2144f5ff41ce6955b3089949782288
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-04-25 15:05:15 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week09/TradingModels.kt
@@ -0,0 +1,8 @@
+package oop_00000113468_MuhammadNabilFirdiansyah.week09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String)
