ba2e12cb4a14d986404e75145d5499975697b559
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-04-28 15:18:00 +0700

    week10: implement generic function with return type

--- src/oop_00000113468_MuhammadNabilFirdiansyah/week10/GenericMethods.kt
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week10/GenericMethods.kt
@@ -3,3 +3,7 @@ package oop_00000113468_MuhammadNabilFirdiansyah.week10
 fun <T> printData(data:T){
     println("data yang diterima : $data")
 }
+
+fun <T>processData(input:T):T{
+    return input
+}
