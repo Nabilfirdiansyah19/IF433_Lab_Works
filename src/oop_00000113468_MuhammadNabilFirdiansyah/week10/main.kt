548bdd340ff09df13a21783bf99e9eea98b49ce0
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-04-28 15:19:43 +0700

    week10: test generic functions with Double and String

--- src/oop_00000113468_MuhammadNabilFirdiansyah/week10/main.kt
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week10/main.kt
@@ -11,4 +11,9 @@ fun main (){
     println("\n=== TEST MULTIPLE PARAMETERS ===")
     val itemPrice= PairBox("Bitcoin",65000)
     println("Aset: ${itemPrice.key}, harga: ${itemPrice.value}USD")
+
+    println("\n=== TEST GENERIC FUNCTIONS ===")
+    println(3.14)
+    val result = processData("stable coin")
+    println("hasil proses: $result")
 }
