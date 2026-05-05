5066f597054467f2d35cd4b3ecfbd414559033ca
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-05-05 15:02:55 +0700

    week11: create string extension functions

---
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week11/StringExtensions.kt
@@ -0,0 +1,9 @@
+package oop_00000113468_MuhammadNabilFirdiansyah.week11
+
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
