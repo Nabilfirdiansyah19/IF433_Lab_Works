abf1cc205914bfc99faa695f0b1ab9f1aa61e819
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-05-12 15:03:43 +0700

    week12: (task) test full robust smart feeder pipeline

--- src/oop_00000113468_MuhammadNabilFirdiansyah/week12/SmartFeeder.kt
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week12/SmartFeeder.kt
@@ -39,4 +39,5 @@ fun main() {
             println("Peringatan ke Pemilik: ${error.message}")
             println("(Opsional: Berikan chicken jerky secara manual)")
         }
+    //berhasil di run
 }
