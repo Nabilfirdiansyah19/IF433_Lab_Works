b646a81ff6559c6096f1510cc256d9ab9f4b1cb6
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-05-12 14:58:46 +0700

    week12: (task) implement food stock validation with throw

--- src/oop_00000113468_MuhammadNabilFirdiansyah/week12/SmartFeeder.kt
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week12/SmartFeeder.kt
@@ -3,6 +3,10 @@ package oop_00000113468_MuhammadNabilFirdiansyah.week12
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
     if (isJammed) throw DispenserJamException()
+    if (requestedGram > availableGram) throw FoodEmptyException(requestedGram, availableGram)
+
+    println("Kibble berhasil dikeluarkan!")
+    return availableGram - requestedGram
     return availableGram // placeholder sementara
 
 }
