515868a3135be3ffe76a33a46a7534d3a8f7f14f
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-05-12 15:00:21 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_00000113468_MuhammadNabilFirdiansyah/week12/SmartFeeder.kt
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week12/SmartFeeder.kt
@@ -13,4 +13,14 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 
 fun main() {
     var currentKibbleStock = 50
+    println("--- JADWAL PAGI ---")
+    try {
+        currentKibbleStock = dispenseKibble(80, currentKibbleStock, false)
+    } catch (e: DispenserJamException) {
+        println("Peringatan: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("Peringatan: ${e.message}")
+    } catch (e: Exception) {
+        println("Peringatan: Terjadi kesalahan tidak terduga.")
+    }
 }
