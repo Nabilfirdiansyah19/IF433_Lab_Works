b3c6baa68200f7eb6c1fddd04de3852ba3de3ac5
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-05-12 14:58:10 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_00000113468_MuhammadNabilFirdiansyah/week12/SmartFeeder.kt
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week12/SmartFeeder.kt
@@ -2,5 +2,7 @@ package oop_00000113468_MuhammadNabilFirdiansyah.week12
 
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+    if (isJammed) throw DispenserJamException()
     return availableGram // placeholder sementara
+
 }
