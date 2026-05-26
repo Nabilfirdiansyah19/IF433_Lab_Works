82e693eb939ea3a2e6e0000e2410c0c7d309d09f
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-05-26 15:20:11 +0700

    week14: (task) implement PricingStrategy interface for OCP

--- src/oop_00000113468_MuhammadNabilFirdiansyah/week14/EcommerceTask.kt
+++ src/oop_00000113468_MuhammadNabilFirdiansyah/week14/EcommerceTask.kt
@@ -1,41 +1,28 @@
 package oop_00000113468_MuhammadNabilFirdiansyah.week14
 
+
 import java.io.File
-
-class BadOrderProcessor {
-    private val file = File("orders.csv")
-
-    fun processOrder(itemName: String, basePrice: Double, customerType: String) {
-        val finalPrice = when (customerType) {
-            "REGULAR" -> basePrice
-            "VIP" -> basePrice * 0.90
-            else -> basePrice
-        }
-
-        println("Memproses pesanan $itemName seharga $finalPrice")
-
-        file.appendText("$itemName, $finalPrice, $customerType\n")
-
-        println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
-    }
-}
 
 interface OrderRepository {
     fun saveOrder(itemName: String, finalPrice: Double, customerType: String)
+}
+
+interface NotificationService {
+    fun sendNotification(itemName: String)
+}
+
+interface PricingStrategy {
+    fun calculate(price: Double): Double
 }
 
 class CsvOrderRepository : OrderRepository {
     private val file = File("orders.csv")
 
     override fun saveOrder(itemName: String, finalPrice: Double, customerType: String) {
-        file.bufferedWriter().use { writer ->
+        file.bufferedWriter().use {
             file.appendText("$itemName, $finalPrice, $customerType\n")
         }
     }
-}
-
-interface NotificationService {
-    fun sendNotification(itemName: String)
 }
 
 class EmailNotifier : NotificationService {
@@ -44,20 +31,48 @@ class EmailNotifier : NotificationService {
     }
 }
 
+class RegularPricing : PricingStrategy {
+    override fun calculate(price: Double): Double = price
+}
+
+class VipPricing : PricingStrategy {
+    override fun calculate(price: Double): Double = price * 0.90
+}
+
 class SafeOrderProcessor(
     private val repo: OrderRepository,
     private val notifier: NotificationService
 ) {
-    fun processOrder(itemName: String, basePrice: Double, customerType: String) {
-        val finalPrice = when (customerType) {
-            "REGULAR" -> basePrice
-            "VIP" -> basePrice * 0.90
-            else -> basePrice
-        }
+    fun processOrder(itemName: String, basePrice: Double, strategy: PricingStrategy, customerType: String) {
+        val finalPrice = strategy.calculate(basePrice)
 
         println("Memproses pesanan $itemName seharga $finalPrice")
 
         repo.saveOrder(itemName, finalPrice, customerType)
         notifier.sendNotification(itemName)
     }
+}
+
+fun main() {
+    val csvRepo = CsvOrderRepository()
+    val emailService = EmailNotifier()
+    val orderProcessor = SafeOrderProcessor(csvRepo, emailService)
+
+    println("--- SIMULASI TRANSAKSI E-COMMERCE ---")
+
+    orderProcessor.processOrder(
+        itemName = "Laptop ASUS Rog",
+        basePrice = 15000000.0,
+        strategy = RegularPricing(),
+        customerType = "REGULAR"
+    )
+
+    println()
+
+    orderProcessor.processOrder(
+        itemName = "iPhone 15 Pro",
+        basePrice = 20000000.0,
+        strategy = VipPricing(),
+        customerType = "VIP"
+    )
 }
